63262ed013d4bcbf73afa2bf3b31f36a55e2f0c0
Callan McNulty <jabberwocky222@gmail.com>
2016-08-05 10:09:42 -0700

    Add links to repo list, add second API call to get other profile data, add display function for profile data, make minor display changes throughout

--- js/lookup-interface.js
+++ js/lookup-interface.js
@@ -1,23 +1,45 @@
 var getRepos = require('./../js/lookup.js').getRepos;
+var getUserInfo = require('./../js/lookup.js').getUserInfo;
 
 displayRepoList = function(repos) {
-  $("#repos").empty();
-
+  $("#repo-list").empty();
+  $("#error").empty();
   if(typeof repos.message==="string") {
-    $("#repos").text("No matching user found");
+    $("#profile-info").empty();
+    $("#error").text("No matching user found");
   } else {
+    $("#repo-list").append(
+      "<h3>Repositories:</h3>"+
+      "<ul id='repos'></ul>"
+    );
     repos.forEach(function(repo) {
-      $("#repos").append("<li>"+repo.name+"</li>");
+      $("#repos").append("<li><a href='"+repo.svn_url+"'>"+repo.name+"</a></li>");
       if(repo.description!=null && repo.description!="") {
         $("#repos").append("<ul><li>"+repo.description+"</li></ul>");
       }
     });
   }
 };
+displayUserInfo = function(user) {
+  $("#profile-info").empty();
+  $("#profile-info").append(
+    "<h3>"+(user.name?user.name:user.login)+"</h3>"+
+    "<a href='"+user.html_url+"'>View on GitHub</a><br><br>"+
+    "<img src='"+user.avatar_url+"'></img>"+
+    (user.bio?"<p>"+user.bio+"</p>":"")+
+    "<ul>"+
+      (user.location?"<li><strong>Location: </strong>"+user.location+"</li>":"")+
+      (user.email?"<li><strong>Email: </strong>"+user.email+"</li>":"")+
+      (user.company?"<li><strong>Company: </strong>"+user.company+"</li>":"")+
+    "</ul>"+
+    "<hr>"
+  );
+};
 
 $(document).ready(function() {
   $("#username-form").submit(function(event) {
     event.preventDefault();
     getRepos($("#username").val(), displayRepoList);
+    getUserInfo($("#username").val(), displayUserInfo);
   });
 });
--- js/lookup.js
+++ js/lookup.js
@@ -1,11 +1,20 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(username, displayRepoList){
+exports.getRepos = function(username, displayRepoList) {
   $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     displayRepoList(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
     displayRepoList(error.responseJSON);
+  });
+};
+
+exports.getUserInfo = function(username, displayUserInfo) {
+  $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    displayUserInfo(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
   });
 };
